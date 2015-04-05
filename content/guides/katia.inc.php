6d006b4e6b60547e6b8bff75c099eb72274704a4
Sergey <sergiorichie@gmail.com>
2015-04-05 16:53:29 -0700

    minor

--- content/guides/katia.inc.php
+++ content/guides/katia.inc.php
@@ -27,13 +27,13 @@
             <li>I do believe that travelling can make you wiser</li>
             <li>I speak English fluently and also improve my German on a daily basis. But to be honest – I would rather learn some rare language. It would rather be Serbian or Latvian :-)</li>
             <li>I lost 13 kg a couple of years ago</li>
-            <li>Sarcasm is my second name. Indeed.</li>
+            <li>Sarcasm is my second name. Indeed</li>
             <li>I adore to meet new people and get acquainted with new cultures</li>
             <li>I love reading. Books are amazing companions</li>
             <li>I know the secret why Russians do not smile that often. But I do :)</li>
             <li>There are various suggestions  about my nationality: from all types of Scandinavian nationalities to Japanese (what?!) mother and Georgian father. Well, to be honest, I am Russian. But my great-great-granddaddy was born in Finland, that’s all what makes me a bit exotic </li>
             <li>I don’t like clubs. Bars are better. Wine and beer. I do know a lot about those ones.</li>
-            <li>Gastronomy is my third name. I know a lot about gastronomic side of Moscow city. My first is Katia. The second? Find out on the tour :)</li>
+            <li>Gastronomy is my third name. I know a lot about gastronomic side of Moscow city. My first name is Katia. The second? Find out on the tour :)</li>
         </ol>
     </section>
 </div>
