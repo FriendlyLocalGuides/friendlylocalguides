84cbabab877245c4077d1bdfba7d01f0e07e283c
Sergey <sergiorichie@gmail.com>
2015-04-05 22:45:49 -0700

    minor

--- content/contact.inc.php
+++ content/contact.inc.php
@@ -1,7 +1,7 @@
 <section class="whiten form-container contact">
     <h5>We'd love to hear from you! If emails are not your thing, get in touch with us on:
         <ul class="contact-items">
-            <li><img src="../i/social_buttons/skype.png" alt="Skype"/>Skype: <strong>friendly.local.guides</strong>,</li>
+            <li><img src="../i/social_buttons/skype.png" alt="Skype"/>Skype: <strong>friendly.local.guides</strong></li>
             <li><img src="../i/social_buttons/whatsapp.png" alt="WhatsApp"/>WhatsApp: <strong>13474355207</strong></li>
         </ul>
     </h5>
