aee0587862277e7d6b5d3d13eba0730e0faaeb6b
Serezha <sergiorichie@gmail.com>
2015-05-25 21:55:11 -0700

    ckeditor

--- content/text-editor/text-editor.php
+++ content/text-editor/text-editor.php
@@ -16,7 +16,7 @@
 
         <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-        <script src="/line-control/editor.js"></script>
+        <script src="/ckeditor/ckeditor.js"></script>
 
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
@@ -24,19 +24,41 @@
         <link rel="stylesheet" href="/css/texteditor.css"/>
         <link rel="stylesheet" href="/css/tours.css"/>
         <script type="text/javascript">
-            $(document).ready( function() {
-                var texteditor = $("#txtEditor").Editor(),
-                    $textarea = $("#txtEditorContent"),
-                    description;
+            $(window).load( function() {
+
+                var editor = CKEDITOR.replace('txtEditor'),
+                    $title_long_container = $('.title-long-container'),
+                    $title_long_txt = $title_long_container.find('.too_long_title'),
+                    $title = $('.title'),
+                    $title_long = $('.title-long'),
+                    $title_for_sending = $('.title-for-sending'),
+                    PATH_FILE_UPLOAD = '/i/tours/',
+                    $splash_screen_value = $('input[name=splashscreen]').val(),
+                    $splash_screen_el = $('.splash-screen');
+
+
+                function title(){
+                    if($title_long.val() != ""){
+                        $title_long_txt.text($title_long.val());
+                        $title_for_sending.val($title.val() + " " + $title_long_container.html());
+                    }else{
+                        $title_for_sending.val($title.val())
+                    }
+                }
+
+                function uploadPhoto(){
+                    $splash_screen_el.attr('src', $splash_screen_value);
+                    console.log($splash_screen_el);
+                }
+
                 $('#add-tour').on('click', function(e){
                     e.preventDefault();
-                    description =  $("#txtEditor").Editor('getText');
-                    $textarea.val(description);
-                    console.log($textarea.val());
+                    title();
+                    uploadPhoto();
+                    CKEDITOR.instances.txtEditor.updateElement();
                     $.post(
                         "/content/tour-creator.php",
                         $("#tour-form").serialize(),function(result){
-//                            console.log($("#tour-form").serialize());
                             console.log(result);
                         }
                     );
@@ -46,18 +68,21 @@
     </head>
     <body>
         <section>
-            <form id="tour-form" class="text-editor">
+            <img class="cover-img" src="" alt="" hidden="hidden" />
+            <img class="splash-screen" src="" alt="" hidden="hidden" />
+            <div class="title-long-container" hidden="hidden"><br/><span class='too_long_title'></span></div>
+            <form id="tour-form" class="text-editor" enctype="multipart/form-data">
                 <div class="input-groups">
                     <input class="form-control" type="text" name="city" placeholder="City"/>
                     <input class="form-control" type="text" name="link" placeholder="Link to the tour"/>
-                    <input type="file" name="cover-img" placeholder="Cover image"/>
-                    <input class="form-control" type="text" name="title" placeholder="Title of the tour"/>
-                    <input class="form-control" type="text" name="title" placeholder="Second part of the title if it's too long"/>
+                    <input type="file" name="coverimg" />
+                    <input class="form-control title" type="text" placeholder="Title of the tour"/>
+                    <input class="form-control title-long" type="text" placeholder="Second part of the title if it's too long"/>
+                    <input class="title-for-sending" type="text" name="title" hidden="hidden"/>
                     <input class="form-control" type="text" name="subtitle" placeholder="subtitle"/>
                     <input class="form-control" type="text" name="price" placeholder="price"/>
-                    <input type="file" name="price" placeholder="Splash Screen"/>
-                    <textarea id="txtEditor"></textarea>
-                    <textarea id="txtEditorContent" name="description" hidden="hidden"></textarea>
+                    <input type="file" name="splashscreen" placeholder="Splash Screen"/>
+                    <textarea id="txtEditor" name="description"></textarea>
                     <ul id="tour-gallery" class="list-unstyled">
                         <li>
                             <input type="file" name="picture" placeholder="Picture"/>
@@ -66,7 +91,6 @@
                         </li>
                     </ul>
                 </div>
-
                 <input id="add-tour" class="btn btn-lg btn-success" value="Publish tour" type="submit"/>
             </form>
         </section>
--- content/tour-creator.php
+++ content/tour-creator.php
@@ -1,36 +1,25 @@
 <?
 try {
     require_once "config.php";
+//    require_once "image_uploader.php";
 
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $currentPage = basename($_SERVER['HTTP_REFERER']);
-        /*$sql_tour = "select * from tours where url = '$currentPage' AND city = '$city'";
-        foreach ($dbh->query($sql_tour) as $row){
-            $titleTour = $row['title'];
-            $subtitleTour = $row['subtitle'];
-            $price = $row['price'];
-            $splashScreen = $row['url_splash_image'];
-            $descriptionTour = $row['description'];
-            $tourGallery = $row['urls_gallery'];
-        }*/
-//        $sql_tour = "select title from tours where url = '$currentPage'";
         $city         = $_POST['city'];
         $url          = $_POST['link'];
         $title        = $_POST['title'];
         $subtitle     = $_POST['subtitle'];
         $price        = $_POST['price'];
+        $splashscreen = $_POST['splashscreen'];
         $description  = $_POST['description'];
-       /* if($_POST['video'] != ""){
-            $video = $_POST['video'];
-        }else{
-            $video = "None";
-        }*/
+
+
 
         /*** превратить все ошибки в исключения ***/
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = "INSERT INTO tours (city, url, title, subtitle, price, description)
-                VALUES (:city, :link, :title, :subtitle, :price, :description)";
+        $sql = "INSERT INTO tours (city, url, title, subtitle, price, url_splash_image, description)
+                VALUES (:city, :link, :title, :subtitle, :price, :splashscreen, :description)";
         /*** готовим выражение ***/
         $stmt = $dbh->prepare($sql);
 
@@ -40,7 +29,8 @@ try {
         $stmt->bindParam(':title', $title, PDO::PARAM_STR);
         $stmt->bindParam(':subtitle', $subtitle, PDO::PARAM_STR);
         $stmt->bindParam(':price', $price, PDO::PARAM_STR);
-        $stmt->bindParam(':description', $price, PDO::PARAM_STR);
+        $stmt->bindParam(':splashscreen', $splashscreen, PDO::PARAM_STR);
+        $stmt->bindParam(':description', $description, PDO::PARAM_STR);
 
         /*** запускаем sql выражение ***/
         if($stmt->execute()){
@@ -55,20 +45,3 @@ $sql = "select * from tours WHERE url = '$url'";
 foreach ($dbh->query($sql) as $row){
     echo $row['city'].' '.$row['url'].' '.$row['title'].' '.$row['subtitle'].' '.$row['price'].' '.$row['description'];
 }
-/*
-if($_POST['refresh']) {
-    populate_shoutbox();
-}
-
-function populate_shoutbox(){
-    global $dbh;
-    global $currentPage;
-    $sql = "select * from reviews where url = '$currentPage' order by date_time desc limit 1";
-    foreach ($dbh->query($sql) as $row){
-        echo '<li class="review">';
-        echo '<img class="rating" src="/i/stars/stars-'.$row['rating'].'.png"/>'.$row['message'];
-        echo '<p class="tourist-writer"><em>'.$row['name'].', ' .$row['country'].', '. date("M d, Y", strtotime($row['date_time'])).'</em></p>';
-        echo '</li>';
-    }
-
-}*/
