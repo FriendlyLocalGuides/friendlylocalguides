54dbeef750a9e511e5b09f7d10bcc0ad407f9eb1
Serezha <sergiorichie@gmail.com>
2015-08-08 00:01:21 -0700

    oohhhhhh

--- Stripe/config.php
+++
@@ -1,7 +0,0 @@
-<?
-require_once('vendor/autoload.php');
-$stripe = array(
-    'secret_key'      => 'sk_live_7oZ22dZGiw1VTFNvOJuDXVBm',
-    'publishable_key' => 'pk_live_tD2JJyEeD449SHD7DX6WGwrj'
-);
-\Stripe\Stripe::setApiKey($stripe['secret_key']);
