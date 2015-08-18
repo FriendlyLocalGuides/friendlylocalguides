bb218ef63dfa50c959cde1d1ba1a525e50f9c972
Serezha <sergiorichie@gmail.com>
2015-08-17 21:43:21 -0700

    404

---
+++ 404-error-page.php
@@ -0,0 +1,89 @@
+
+<meta content="width=device-width" name="viewport">
+<link href='//fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+<style>
+    html {
+        color: #141414;
+        display: table;
+        font-family: "Arial",sans-serif;
+        height: 100%;
+        text-align: center;
+        width: 100%;
+    }
+    body{
+        margin: 15em auto;
+    }
+    h1, h2{
+        font-family: "Arial",sans-serif;
+        font-weight: 800;
+        letter-spacing: 3px;
+        margin-bottom: 1em;
+    }
+    h1{
+        font-size: 70px;
+        margin-bottom: 20px;
+    }
+    h2{
+        font-size: 20px;
+        margin-bottom: 40px;
+    }
+    p{
+        color: #202020;
+        font-family: "PT Sans",sans-serif;
+        font-size: 23px;
+        margin: 0 auto;
+        /*width: 600px;*/
+    }
+
+    .btn{
+        /*color: #1d1d1d;*/
+        color: #fff;
+        font-family: "Kaushan Script",cursive;
+        background: rgba(0, 128, 0, 0.9);
+        display: inline-block;
+        font-size: 25px;
+        font-weight: 400;
+        /*letter-spacing: 2px;*/
+        margin-top: 3em;
+        padding: 10px 25px;
+        position: relative;
+        text-align: left;
+        text-decoration: none;
+        vertical-align: middle;
+        transition: transform 0.18s cubic-bezier(0.25, 0, 0.3, 1) 0s, box-shadow 0.18s;
+        z-index: 1;
+        box-shadow: 0 10px 5px rgba(0, 0, 0, .15);
+    }
+
+    .btn:hover{
+        transform: scale3d(1.1, 1.1, 1.1);
+        box-shadow: 0 5px 5px rgba(0, 0, 0, .1);
+    }
+
+    @media screen and (max-width:768px) {
+        body{
+            display: table-cell;
+            vertical-align: middle;
+            margin: auto;
+        }
+        h1{
+            font-size: 43px;
+            margin-top: -25px;
+        }
+        h2{
+            font-size: 15px;
+        }
+
+        p{
+            font-size: 17px;
+            padding: 0 10px;
+        }
+        .btn{
+            font-size: 21px;
+        }
+    }
+</style>
+<h1>ERROR 404</h1>
+<h2>PAGE NOT FOUND</h2>
+<p>Sorry, but the page you were trying to view does not exist.</p>
+<a class="btn" href="https://friendlylocalguides.com/">Back to Home Page</a>
