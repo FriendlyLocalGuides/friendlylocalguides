ecfdb78460d44a48cfc2be7d5b24a5fae4bb128b
Serezha <sergiorichie@gmail.com>
2015-08-21 21:11:16 -0700

    sitemap added

---
+++ sitemap.php
@@ -0,0 +1,33 @@
+<div class="sitemap-page">
+    <section class="content_box sitemap height-viewport">
+        <div class="heading-decoration">
+            <h1>Sitemap</h1>
+            <div class="decor">
+                <div class="decor-circle"></div>
+            </div>
+        </div>
+        <ul class="sitemap-list">
+            <li>
+                <a href="https://friendlylocalguides.com/" title="Main Page">Main page</a>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/moscow/tours" title="Moscow tours">Moscow tours</a>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/saint-petersburg/tours" title="Saint-Petersburg tours">Saint-Petersburg tours</a>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/moscow/guides" title="Guides">Guides</a>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/about" title="About">About</a>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/contact" title="Contact">Contact</a>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/blog/" title="Blog">Blog</a>
+            </li>
+        </ul>
+    </section>
+</div>
