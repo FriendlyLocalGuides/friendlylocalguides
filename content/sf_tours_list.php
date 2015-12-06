6155768b292aa6ea4b3dbfa9dee8fdae3e6f9e14
sergiusmuzzz <vivaldi21>
2015-12-05 17:28:41 -0800

    updated sf tours page

--- content/sf_tours_list.php
+++ content/sf_tours_list.php
@@ -42,7 +42,7 @@
         <div class="pane features extra-features height-viewport" style="display: none;">
             <ul class="list-of-features">
                 <li class="feature-1">
-                    <span>Meet a local and make a new friend in San Francisco. Choose your own San Francisco tour guide, see the photos and read the fun facts of our San Francisco guides. We can even organize a Skype call with your private guide once you book the tour. Ask your guide questions about any subject and get genuinely connected with your new local friend.</span>
+                    <span>Meet a local and make a new friend in San Francisco. Choose your own San Francisco tour guide, see the photos and read the fun facts. We can even organize a Skype call with your private guide once you book the tour. Ask your guide questions about any subject and get genuinely connected with your new local friend.</span>
                 </li>
                 <li class="feature-2">
                     <span>Exciting fun communication with a local on your San Francisco guided tour, not just boring history lessons. Your personal tour guide focuses solely on you and makes sure you get the most out of your San Francisco experience.</span>
@@ -57,7 +57,7 @@
                     <span>Flexibility during your tour: changes can be made at any time to suit your individual preferences. Go at your own pace and decide when to stop for a coffee break.</span>
                 </li>
                 <li class="feature-6">
-                    <span>San Francisco Must See + Off The Beaten Path. We will help you make your San Francisco tour a unique lifetime experience combining all the most historical and iconic places of San Francisco, as well as non-touristy places and insider’s cozy nooks only locals knows about. Just give us a hint and tell us what interests you and why you chose San Francisco as your travel destination. Your San Francisco private guide will make sure you have an authentic experience of local life, if you want to.</span>
+                    <span>San Francisco Must See + Off The Beaten Path. We will help you make your San Francisco tour a unique lifetime experience combining all the most historical and iconic places, as well as non-touristy places and insider’s cozy nooks only locals knows about. Just give us a hint and tell us what interests you and why you chose San Francisco as your travel destination. Your San Francisco private guide will make sure you have an authentic experience of local life, if you want to.</span>
                 </li>
                 <li class="feature-7">
                     <span>Reliable, friendly and flexible online service. Your personal travel consultant is at your service any time via email, facebook, skype or whatsapp. You will not feel alone with Friendly Local Guides. Ask us any questions about tours in San Francisco, what to do in San Francisco, where to go in San Francisco at night, what’s on in San Francisco and any other question you have.</span>
@@ -66,10 +66,10 @@
                     <span>Tour San Francisco in style. All our guides are good photographers and will gladly take photos of you and with you amongst spectacular San Francisco scenery that can be treasured for a lifetime.</span>
                 </li>
                 <li class="feature-9">
-                    <span>Traditional American cuisine. You don’t have to go for Food Tour of San Francisco to try the best American food in the city. On your tour you will experience American hospitality and national dishes in local cozy restaurants with traditional cuisine and interior.</span>
+                    <span>Traditional American cuisine. On your tour you will try the best American food in the city experience national dishes in local cozy restaurants with traditional cuisine and interior.</span>
                 </li>
                 <li class="feature-10">
-                    <span>Expert local advice. Apart from super friendly San Francisco sightseeing tours, we are also good at advising how to make the most of your time in San Francisco. Best coffee shops in the city, best hotels, reliable and affordable taxi service etc. Just ask us!</span>                </li>
+                    <span>Expert local advice. Apart from super friendly San Francisco sightseeing tours, we are also good at advising how to make the most of your time in th city. Best coffee shops, best hotels, reliable and affordable taxi service etc. Just ask us!</span>                </li>
             </ul>
         </div>
     </div>
