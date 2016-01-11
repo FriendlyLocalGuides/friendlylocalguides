a38c134354c23b03b443fc16ee9f419075975e94
sergiusmuzzz <vivaldi21>
2016-01-10 22:34:09 -0800

    skeleton of navigation for FLG (added files)

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('pages.main');
+    }
+    public function about()
+    {
+        return view('pages.about');
+    }
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+}
