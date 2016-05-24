9f8ba94332cc7070c97b1c5719da3e630ca18616
sergey <sergiorichie@gmail.com>
2016-05-23 20:13:31 -0700

    added and updated sitemap.php

---
+++ sitemap.php
@@ -0,0 +1,163 @@
+<div class="sitemap-page">
+    <section class="content_box sitemap height-viewport">
+        <div class="heading-decoration">
+            <h1>Sitemap</h1>
+            <div class="decor">
+                <div class="decor-circle"></div>
+            </div>
+        </div>
+        <ul class="sitemap-list">
+            <li>
+                <a href="https://friendlylocalguides.com/" title="Main Page">Main page</a>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/moscow/tours" title="Moscow tours">Moscow tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/layover-tour-moscow">Moscow layover tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/soviet-tour-russia">Soviet tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/bus-tour-in-moscow">Bus tour of Moscow</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/photo-tour-of-moscow">Photo tour of Moscow</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/bike-tour-in-moscow">Bike tour in Moscow</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/boat-tour-moscow">Boat tour in Moscow</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/ultimate-tour-of-moscow-with-traditional-cuisines">Ultimate tour of Moscow</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/golden-ring-tour">Golden Ring tour of Sergiev posad</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/food-tour-moscow-st-petersburg">Food tour in Moscow</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/moscow-private-tour">Tretyakov Gallery tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/metro-tour-moscow">Moscow Metro tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/jewish-tour-moscow">Jewish tour in Moscow</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/free-tour">Moscow free walking tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/kremlin-tour-moscow">Moscow Kremlin tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/red-square-and-city-tour-moscow">Moscow city & Red square tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/going-out-in-moscow">Moscow Off the beaten path tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/izmailovo-kremlin-tour">Izmailovo Flea Market & Kremlin in Izmailovo</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/top-20-must-see-in-moscow">Moscow must see tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/2-days-in-moscow">2 Days in Moscow</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/3-days-in-moscow">3 Days in Moscow</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/tours/moscow-at-night">Moscow at night</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/saint-petersburg/tours" title="Saint-Petersburg tours">Saint-Petersburg tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/top-20-must-see-in-saint-petersburg">Saint Petersburg must see tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/2-day-tour-of-saint-petersburg">2 Days in Saint Petersburg</a></li>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/3-day-tour-of-saint-petersburg">3 Days in Saint Petersburg</a></li>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/st-petersburg-walking-tour">St Petersburg walking tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/st-petersburg-private-tour">5-hour City tour of Saint Petersburg</a></li>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/boat-trip-tour">Shore tours in Saint Petersburg</a></li>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/peter-and-paul-tour">Peter & Paul Fortress tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/peterhof-tour">Peterhof tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/st-petersburg-and-moscow-tours">St Petersburg & Moscow tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/tours/food-tour-st-petersburg">Food tour in Saint Petersburg</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/san-francisco/tours" title="San Francisco tours">San Francisco tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/san-francisco/tours/san-francisco-tour">San Francisco driving tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/san-francisco/tours/san-francisco-city-tour">San Francisco in 1 day tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/san-francisco/tours/2-days-in-san-francisco">2 day tour in San Francisco</a></li>
+                    <li><a href="https://friendlylocalguides.com/san-francisco/tours/3-days-in-san-francisco">3 day tour in San Francisco</a></li>
+                    <li><a href="https://friendlylocalguides.com/san-francisco/tours/san-francisco-walking-tours">San Francisco walking tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/san-francisco/tours/san-francisco-bike-tours">San Francisco bike tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/san-francisco/tours/san-francisco-night-tour">San Francisco Night Tour</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/chicago/tours" title="Chicago tours">Chicago tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/chicago/tours/chicago-tour">1 day tour in Chicago</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/lisbon/tours" title="Lisbon tours">Lisbon tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/lisbon/tours/lisbon-tour">1 day tour in Lisbon</a></li>
+                    <li><a href="https://friendlylocalguides.com/lisbon/tours/lisbon-city-tour">Lisbon City Tour</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/los-angeles/tours" title="Los Angeles tours">Los Angeles tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/los-angeles/tours/los-angeles-one-day-tour">1 day tour in Los Angeles</a></li>
+                    <li><a href="https://friendlylocalguides.com/los-angeles/tours/2-days-in-los-angeles">2 Days Tour in Los Angeles</a></li>
+                    <li><a href="https://friendlylocalguides.com/los-angeles/tours/3-days-in-los-angeles">3 Days Tour in Los Angeles</a></li>
+                    <li><a href="https://friendlylocalguides.com/los-angeles/tours/hollywood-tour">Hollywood city tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/los-angeles/tours/los-angeles-city-tour">Los Angeles City Tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/los-angeles/tours/los-angeles-driving-tour">Los Angeles Driving Tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/los-angeles/tours/los-angeles-night-tour">Los Angeles Night Tour</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/washington/tours" title="Washington Tours">Washington Tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/washington/tours/washington-tour">1 Day Tour in Washington</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/milan/tours" title="Milan tours">Milan tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/milan/tours/milan-tour">1 day tour in Milan</a></li>
+                    <li><a href="https://friendlylocalguides.com/milan/tours/2-days-in-milan">2 Days Tour in Milan</a></li>
+                    <li><a href="https://friendlylocalguides.com/milan/tours/3-days-in-milan">3 Days Tour in Milan</a></li>
+                    <li><a href="https://friendlylocalguides.com/milan/tours/milan-city-tour">Milan City Tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/milan/tours/milan-food-tour">Milan Food Tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/milan/tours/milan-night-tour">Milan Night Tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/milan/tours/milan-photo-walking-tour">Walking tour in Milan</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/new-york/tours" title="New York tours">New York tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/new-york/tours/new-york-tour">1 day tour in New York</a></li>
+                    <li><a href="https://friendlylocalguides.com/new-york/tours/2-days-in-new-york">2 Days Tour in New York</a></li>
+                    <li><a href="https://friendlylocalguides.com/new-york/tours/3-days-in-new-york">3 Days Tour in New York</a></li>
+                    <li><a href="https://friendlylocalguides.com/new-york/tours/new-york-city-night-tour">New York Night Tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/new-york/tours/new-york-city-tour">Private New York City Tour</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/paris/tours" title="Paris tours">Paris tours</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/paris/tours/one-day-in-paris">1 Day in Paris</a></li>
+                    <li><a href="https://friendlylocalguides.com/paris/tours/2-days-in-paris">2 Days tour in Paris</a></li>
+                    <li><a href="https://friendlylocalguides.com/paris/tours/3-days-in-paris">3 Days In Paris</a></li>
+                    <li><a href="https://friendlylocalguides.com/paris/tours/paris-city-tour">Paris City Tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/paris/tours/montmartre-tour-in-paris">Montmartre Tour
+                            Romantic Paris</a></li>
+                    <li><a href="https://friendlylocalguides.com/paris/tours/paris-food-tour">Paris Food Tour</a></li>
+                    <li><a href="https://friendlylocalguides.com/paris/tours/paris-night-tour">Paris Night Tour</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/moscow/guides" title="Guides">Moscow Guides</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/moscow/guides/vera">Vera</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/guides/katya">Katia</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/guides/karina">Karina</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/guides/inna">Inna</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/guides/dasha">Dasha</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/guides/anya">Anya</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/guides/anna">Anna</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/guides/angelina">Angelina</a></li>
+                    <li><a href="https://friendlylocalguides.com/moscow/guides/alina">Alina</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/saint-petersburg/guides" title="Guides">St. Petersburg Guides</a>
+                <ul>
+                    <li><a href="https://friendlylocalguides.com/saint-petersburg/guides/valery">Valery</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/about" title="About">About</a>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/contact" title="Contact">Contact</a>
+            </li>
+            <li>
+                <a href="https://friendlylocalguides.com/blog/" title="Blog">Blog</a>
+            </li>
+        </ul>
+    </section>
+</div>
