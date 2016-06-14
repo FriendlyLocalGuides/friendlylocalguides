6663aa240d87b8aa47f545831a083dc0f27c8040
sergey <sergiorichie@gmail.com>
2016-06-13 20:41:05 -0700

    added phone number

--- content/contact.inc.php
+++ content/contact.inc.php
@@ -4,6 +4,7 @@
         <ul class="contact-items">
             <li><img src="../i/social_buttons/skype.png" alt="Skype"/>Skype: <strong>friendly.local.guides</strong></li>
             <li><img src="../i/social_buttons/whatsapp.png" alt="WhatsApp"/>WhatsApp: <strong>13474355207</strong></li>
+            <li style="margin-left: 5px;"><i style="font-size: 32px;" class="fa fa-phone" aria-hidden="true"></i>Phone: <strong>+7 926 728-57-86</strong></li>
             <li class="contact-us"><i class="fa fa-clock-o"></i><strong>Contact us 24/7</strong></li>
         </ul>
     </h5>
